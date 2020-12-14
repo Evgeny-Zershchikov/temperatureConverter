9a0d9fbd0c5d9cffab04bf2ee6777e239b52660a
Evgeny-Zershchikov <art772@yandex.ru>
2020-12-14 14:08:13 +0300

    lesson-1

---
+++ ArryaBubble.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+        int [] mas = {54, 1, 18, 5, 20, 35,41};
+
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length-1; i++) {
+                if(mas[i] > mas[i+1]){
+                    isSorted = false;
+
+                    buf = mas[i];
+                    mas[i] = mas[i+1];
+                    mas[i+1] = buf;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(mas));
+    }
+}
